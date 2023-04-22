5bdfc399c8359de41aeab8aa67e4c6d2880bb28c
WarLock-0 <imcmoney01@gmail.com>
2023-04-22 16:59:45 +0200

    Inizio

---
+++ siw-OrderManagement/src/main/java/it/uniroma3/siw/SiwOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwOrderManagementApplication.class, args);
+	}
+
+}
